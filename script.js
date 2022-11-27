77733b1782a7f5ff06091ac5643cb59cf979a7de
pekoisgood <peko.claire@gmail.com>
2022-11-28 00:43:05 +0800

    first upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+document.getElementById("navbar-toggler").addEventListener("blur", outFocus());
+
+function outFocus(){
+    document.getElementById("navbarSupportedContent").collapse();
+};
+
+function showImage(imgName) {
+    document.getElementById('filmImg').src = imgName;
+    showLargeImagePanel();
+    unselectAll();
+}
+function showLargeImagePanel() {
+    document.getElementById('filmImgPanel').style.visibility = 'visible';
+}
+    
+function unselectAll() {
+    if(document.selection) document.selection.empty();
+    if(window.getSelection) window.getSelection().removeAllRanges();
+}
+    
+function hideMe(obj) {
+    obj.style.visibility = 'hidden';
+}
+*/
---
+++ snippet/aboutme/script.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("button-addon2");
+var input = document.getElementById("inputName");
+
+input.addEventListener("keypress", function onEvent(event){
+    if (event.key === "Enter" ){
+        event.preventDefault();
+        button.click();
+    }
+});
+
+function sayHello(){
+    let name = document.getElementById("inputName").value;
+    alert("Nice to meet you "+ name + " ! ٩(๑❛ᴗ❛๑)۶");
+};
